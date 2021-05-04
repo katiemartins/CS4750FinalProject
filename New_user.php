7ad9d569b09adabd3acef2a09da73ae0c8332b8a
Katie Martins <kem5en@virginia.edu>
2021-05-03 20:48:43 -0400

    new user edits

--- New_user.php
+++ New_user.php
@@ -18,31 +18,35 @@ if ($_SERVER["REQUEST_METHOD"]== "POST") {
      
     $username = test_input($_POST["username"]);
     $password = test_input($_POST["password"]);
-    $stmt = $conn->prepare("SELECT * FROM UserLogin");
-    $stmt->execute();
-    $users = $stmt->fetchAll();
      
     $sql = "INSERT INTO UserLogin (Name, Password) VALUES ($username, $password)";
 
     $user_u = $conn->prepare("SELECT * FROM UserLogin WHERE Name='$username'");
     $pass_u = $conn->prepare("SELECT * FROM UserLogin WHERE Password='$password'");
+    $user_u->execute();
+    $pass_u->execute();
+    $users = $user_u->fetchAll();
+    $passes = $pass_u->fetchAll();
     
-    
-    if (mysqli_num_rows($user_u) > 0) {
-        echo "<script language='javascript'>";
-        echo "alert('USERNAME TAKEN')";
-        echo "</script>";
-        die();
+    foreach($users as $use){
+        if($use['username'] == $username) {
+            echo "<script language='javascript'>";
+            echo "alert('USERNAME TAKEN')";
+            echo "</script>";
+             die();    
+        }
     }
-    else if (mysqli_num_rows($pass_u) > 0) {
-        echo "<script language='javascript'>";
-        echo "alert('PASSWORD TAKEN')";
-        echo "</script>";
-        die();
+
+    foreach($passes as $pass){
+        if($pass['password'] == $password) {
+            echo "<script language='javascript'>";
+            echo "alert('PASSWORD TAKEN')";
+            echo "</script>";
+            die();  
     }
-    else{
-        $query = mysqli_query($conn, $sql);
-        header("Location: User_page.html");
-    }
+}
+    $query = mysqli_query($conn, $sql);
+    header("Location: User_page.html");
+
 }
 ?>
