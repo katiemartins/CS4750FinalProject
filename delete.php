0d2021a5af34c45b386cd66948fc04b3e233f10b
Katie Martins <kem5en@virginia.edu>
2021-05-03 23:18:11 -0400

    ENNNNNDING

--- delete.php
+++ delete.php
@@ -9,7 +9,7 @@ function test_input($data) {
     return $data;
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "DELETE"){
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $table = test_input($_POST["table"]);
     $attribute = test_input($_POST["attribute"]);
     $value = test_input($_POST["value"]);
@@ -17,35 +17,35 @@ if ($_SERVER["REQUEST_METHOD"] == "DELETE"){
     $sql = "";
     
     if ($comparison == "less_than"){
-        $sql = "DELETE FROM $table WHERE $attribute < $value LIMIT $limit";
+        $sql = "DELETE FROM $table WHERE $attribute < $value";
         $query = $conn->prepare($sql);
         $query->execute();
         header("Location: Admin_page.html");
     }
     else if ($comparison == "greater_than"){
-        $sql = "DELETE FROM $table WHERE $attribute > $value LIMIT $limit";
+        $sql = "DELETE FROM $table WHERE $attribute > $value";
         $query = $conn->prepare($sql);
         $query->execute();
         header("Location: Admin_page.html");
     }
     else if ($comparison == "less_than_or_equal"){
-        $sql = "DELETE FROM $table WHERE $attribute <= $value LIMIT $limit";
+        $sql = "DELETE FROM $table WHERE $attribute <= $value";
         $query = $conn->prepare($sql);
         $query->execute();
         header("Location: Admin_page.html");
     }
     else if ($comparison == "greater_than_or_equal"){
-        $sql = "DELETE FROM $table WHERE $attribute >= $value LIMIT $limit";
+        $sql = "DELETE FROM $table WHERE $attribute >= $value";
         $query = $conn->prepare($sql);
         $query->execute();
         header("Location: Admin_page.html");
     }
     else if ($comparison == "equal"){
         if(gettype($value) == "string"){
-            $sql = "DELETE FROM $table WHERE $attribute LIKE '%$value%' LIMIT $limit";
+            $sql = "DELETE FROM $table WHERE $attribute LIKE '%$value%'";
         }
         else{
-            $sql = "DELETE FROM $table WHERE $attribute = $value LIMIT $limit";
+            $sql = "DELETE FROM $table WHERE $attribute = $value";
         }$query = $conn->prepare($sql);
         $query->execute();
         header("Location: Admin_page.html");
